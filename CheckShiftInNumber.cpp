d5d4ed8677034df2557bc2e1355752a5e3601354
Dimitr119 <53143818+Dimitr119@users.noreply.github.com>
2019-08-17 21:07:27 +0300

    answer whether bit is setted

--- CheckShiftInNumber.cpp
+++ CheckShiftInNumber.cpp
@@ -7,7 +7,7 @@ int main() {
   std::cout << "Please, enter number and bit shift, separated by a space: (I will find, if the bit is setted): ";
   std::cin >> userVar >> userShift;
   bool isBitSetted = (((userVar >> (userShift > 0)) && (userShift - 1)) & 1);
-  std::cout << (isBitSetted ? "No\n" : "Yes\n");
+  std::cout << (isBitSetted ? "No\n" : "Yess\n");
 
   return 0;
 }
