932b011c138ca4fd51a84b1068db12c8ac84a7f6
Dimitr119 <53143818+Dimitr119@users.noreply.github.com>
2019-08-17 21:06:07 +0300

    условие изменено

--- CheckShiftInNumber.cpp
+++ CheckShiftInNumber.cpp
@@ -6,8 +6,8 @@ int main() {
   uint16_t userShift;
   std::cout << "Please, enter number and bit shift, separated by a space: (I will find, if the bit is setted): ";
   std::cin >> userVar >> userShift;
-  bool isBitSetted = (((userVar >> (userShift - 1)) & 1) && (userShift > 0));
-  std::cout << (isBitSetted ? "Yes\n" : "No\n");
+  bool isBitSetted = (((userVar >> (userShift > 0)) && (userShift - 1)) & 1);
+  std::cout << (isBitSetted ? "No\n" : "Yes\n");
 
   return 0;
 }
